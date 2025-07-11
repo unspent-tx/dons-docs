a20e0400332d1d7c238dbf035ce1f8676d3e9859
unspent <205478529+unspent-tx@users.noreply.github.com>
2025-07-11 01:08:09 -0400

    styles

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -13,7 +13,7 @@ export default function Header({ stats, onRefresh, isLoading }: HeaderProps) {
       <div className="">
         <div className="flex flex-wrap items-center justify-between gap-4">
           <div className="">
-            <h1 className="font-bold whitespace-nowrap text-7xl text-neutral-300">
+            <h1 className="font-bold whitespace-nowrap text-7xl text-neutral-100">
               Don's Docs
             </h1>
           </div>
